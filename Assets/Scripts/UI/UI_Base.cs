46d7a6b567131a35339c799c4ac525276523dc06
jinwoo jung <alamash@naver.com>
2024-08-16 17:32:48 +0900

    TMP 추가

--- Assets/Scripts/UI/UI_Base.cs
+++ Assets/Scripts/UI/UI_Base.cs
@@ -85,8 +85,8 @@ public abstract class UI_Base : MonoBehaviour
 
     public void SetResolution()
     {
-        int setWidth = 1080; // 사용자 설정 너비
-        int setHeight = 1920; // 사용자 설정 높이
+        int setWidth = 1440; // 사용자 설정 너비
+        int setHeight = 2920; // 사용자 설정 높이
 
         int deviceWidth = Screen.width; // 기기 너비 저장
         int deviceHeight = Screen.height; // 기기 높이 저장
